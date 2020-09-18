6f9ee61decb552d3eb0c3bf89b8adaee5dc8180f
Aymen BEN MILED <aymen.ben-miled@momentum-tech.ca>
2020-09-18 09:11:29 +0100

    créer le service et les variable environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl:'URL_DE_API',
+  keycloak: {
+    url: 'http://localhost:8080/auth',
+    realm: 'NOM_DU_ROYAUME',
+    clientId: 'CLIENT_ID',
+  }
 };
 
 /*
